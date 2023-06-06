6ed80a44a5a59411702e519a84d4ae50da398466
Amy Gori <amydgori@gmail.com>
2023-06-05 21:51:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import bookData from './book-data.json'
 
 function App() {
   return (
-    <div className="section">
+    <div>
       <h1>Freeshelf</h1>
       {/* Use bookData to show books on the page*/}
     </div>
